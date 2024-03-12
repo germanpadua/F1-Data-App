1ee6b1846001c7ac5a067d64db1ed8b769c70a8d
germanpadua <german8adaba@gmail.com>
2024-03-12 17:16:58 +0100

    Valores por defecto en los desplegables

--- main_app.py
+++ main_app.py
@@ -8,7 +8,7 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from fastf1 import get_event_schedule
-
+from datetime import datetime
 
 
 # Directorio de caché
@@ -37,23 +37,33 @@ opcion_grafico = st.sidebar.selectbox(
     ('Evolución de las posiciones', 'Tiempos de vuelta')
 )
 
+# Lista de años disponibles para la selección
+years = [2020, 2021, 2022, 2023, 2024]
+current_year = datetime.now().year
+default_year_index = years.index(current_year) if current_year in years else len(years) - 1
+
 # Lógica para mostrar el gráfico basado en la elección
 if opcion_grafico == 'Evolución de las posiciones':
     # Cargar datos específicos si es necesario
     # Código para mostrar el gráfico de evolución de las posiciones
     # Selección de parámetros por el usuario
-    year = st.sidebar.selectbox('Selecciona el año', [2020, 2021, 2022, 2023, 2024])
-    # Obtén el calendario de eventos para el año seleccionado
-    calendar = get_event_schedule(year)
+    
+    year = st.sidebar.selectbox('Selecciona el año', years, index=default_year_index)
+    
+    schedule = get_event_schedule(year)
 
-    # Extrae los nombres de los Grandes Premios del calendario
-    gps_disponibles = [event['EventName'] for event in calendar]
+    # Extrae los nombres de los Grandes Premios de la programación
+    gps_disponibles = schedule['EventName'].tolist()
 
     # Crea un selectbox para que el usuario seleccione el GP
-    gp_selected = st.sidebar.selectbox('Selecciona el Gran Premio', gps_disponibles)
+    default_gp_index = gps_disponibles.index('Bahrain Grand Prix') if 'Bahrain Grand Prix' in gps_disponibles else 0
+
+    # Crea un selectbox para que el usuario seleccione el GP, con un valor por defecto
+    gp_selected = st.sidebar.selectbox('Selecciona el Gran Premio', gps_disponibles, index=default_gp_index)
+
     session_type = 'R'  # Para análisis de carrera
 
-    session = cargar_datos_de_sesion(year, gp, session_type)
+    session = cargar_datos_de_sesion(year, gp_selected, session_type)
 
     # Verifica si hay datos cargados para la sesión
     if session.laps.empty:
@@ -79,7 +89,7 @@ if opcion_grafico == 'Evolución de las posiciones':
                                     marker=dict(color=color, size=2)))  # Ajusta el tamaño aquí
 
         # Configura el layout del gráfico
-        fig.update_layout(title=f'Evolución de las Posiciones - {gp} {year}',
+        fig.update_layout(title=f'Evolución de las Posiciones - {gp_selected} {year}',
                         xaxis_title='Número de Vuelta',
                         yaxis_title='Posición',
                         yaxis=dict(autorange="reversed"),  # Invierte el eje Y para que la posición 1 esté arriba
@@ -90,18 +100,23 @@ if opcion_grafico == 'Evolución de las posiciones':
         st.plotly_chart(fig)
 elif opcion_grafico == 'Tiempos de vuelta':
     # Selección de parámetros por el usuario
-    year = st.sidebar.selectbox('Selecciona el año', [2020, 2021, 2022, 2023, 2024])
-    # Obtén el calendario de eventos para el año seleccionado
-    calendar = get_event_schedule(year)
+    year = st.sidebar.selectbox('Selecciona el año', years, index=default_year_index)
+    
+    schedule = get_event_schedule(year)
 
-    # Extrae los nombres de los Grandes Premios del calendario
-    gps_disponibles = [event['EventName'] for event in calendar]
+    # Extrae los nombres de los Grandes Premios de la programación
+    gps_disponibles = schedule['EventName'].tolist()
 
     # Crea un selectbox para que el usuario seleccione el GP
-    gp_selected = st.sidebar.selectbox('Selecciona el Gran Premio', gps_disponibles)
+    default_gp_index = gps_disponibles.index('Bahrain Grand Prix') if 'Bahrain Grand Prix' in gps_disponibles else 0
+
+    # Crea un selectbox para que el usuario seleccione el GP, con un valor por defecto
+    gp_selected = st.sidebar.selectbox('Selecciona el Gran Premio', gps_disponibles, index=default_gp_index)
+
+
     session_type = st.sidebar.selectbox('Selecciona el tipo de sesión', ['FP1', 'FP2', 'FP3', 'Q', 'R'])
 
-    session = cargar_datos_de_sesion(year, gp, session_type)
+    session = cargar_datos_de_sesion(year, gp_selected, session_type)
 
     if session.laps.empty:
         st.error("No se encontraron datos para esta sesión.")
