24387c7d91313193f96b0e356f1313bfae33a095
germanpadua <german8adaba@gmail.com>
2024-08-08 18:10:26 +0200

    Borrado setup_mpl

--- modules/utils.py
+++ modules/utils.py
@@ -7,7 +7,6 @@ def configurar_cache(cache_dir):
     if not os.path.exists(cache_dir):
         os.makedirs(cache_dir)
     fastf1.Cache.enable_cache(cache_dir)
-    plotting.setup_mpl(misc_mpl_mods=False)
     
 def rotate(xy, *, angle):
     rot_mat = np.array([[np.cos(angle), np.sin(angle)],
